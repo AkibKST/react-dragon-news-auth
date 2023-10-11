0bfda372c6efd749d297ee9220af671dfed939a8
AkibKST <akibkst22@gmail.com>
2023-10-11 23:16:28 +0600

    Custom tailwind class and google font set up, Homepage layout and date time formating using momentjs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,24 @@
+import Header from "../shared/Header/Header";
+import LeftSideNav from "../shared/LeftSideNav/LeftSideNav";
+import Navbar from "../shared/Navbar/Navbar";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
 
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This is Home</h2>
+            <Header></Header>
+            <Navbar></Navbar>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2 border">
+                    <h2 className="text-2xl">News coming soon...</h2>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -0,0 +1,11 @@
+
+
+const LeftSideNav = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Left Side Navbar</h2>
+        </div>
+    );
+};
+
+export default LeftSideNav;<h2 className="text-2xl">Left Side Navbar</h2>
