7cc540d72fd695462695db284cb9fb8ec2c54f58
AkibKST <akibkst22@gmail.com>
2023-10-12 22:02:28 +0600

    Added Register page with form data to collect data, login function, register function

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import Navbar from "../shared/Navbar/Navbar";
+
+const Register = () => {
+
+    // Register Button function
+    const handleRegister = (e) =>{
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('email'));
+    }
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>
+                <h2 className="text-3xl my-10 text-center">Please Register</h2>
+                <form onSubmit={handleRegister} className="md:w-3/4 lg:w-1/2 mx-auto mb-2">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" required name="name" placeholder="Name" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" required name="photo" placeholder="Photo URL" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" required name="email" placeholder="Email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Register</button>
+                    </div>
+                </form>
+                <p className="mx-auto text-center">
+                    Already have an account? <Link to="/login" className="text-blue-600 font-bold">Login</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
